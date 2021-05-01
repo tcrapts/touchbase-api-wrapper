0259b2d917ab738a87a2efe65694ba2832e794b4
Terry Crapts <tcrapts@gmail.com>
2021-05-01 19:54:18 +0200

    simplify

--- run.py
+++ run.py
@@ -6,16 +6,18 @@ load_dotenv()
 
 df = pd.read_csv('files/demo.csv')
 
-endpoint = 'http://api.touchbase.report/api/report/'
-access_key_id = os.getenv('ACCESS_KEY_ID')
-access_key = os.getenv('ACCESS_KEY')
-payload = {
-    'id_headers': [],
-    'table':df.to_json(orient='records'),
-    'report_path': ['Demo'],
-    'report_name': 'Demo'
-}
-headers = {'access_key_id': access_key_id, 'access_key': access_key}
-r = requests.post(endpoint, json=payload, headers=headers)
-print('Report pushed to Touchbase with status: ' + str(r.status_code) + '; ' + str(r.text))
+def push_csv():
+    endpoint = 'http://api.touchbase.report/api/report/'
+    access_key_id = os.getenv('ACCESS_KEY_ID')
+    access_key = os.getenv('ACCESS_KEY')
+    payload = {
+        'id_headers': [],
+        'table':df.to_json(orient='records'),
+        'report_path': ['Demo'],
+        'report_name': 'Demo'
+    }
+    headers = {'access_key_id': access_key_id, 'access_key': access_key}
+    r = requests.post(endpoint, json=payload, headers=headers)
+    print('Report pushed to Touchbase with status: ' + str(r.status_code) + '; ' + str(r.text))
 
+push_csv()
